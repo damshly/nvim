96b29ae1ca59bf95b32952754e3d76cd272d23df
damshly <damshly@proton.me>
2026-02-02 19:10:52 +0400

    add svelte lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,34 +7,51 @@ return {
       local lspconfig = require "lspconfig"
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      -- 1. إعداد سيرفر Svelte الأساسي
+      lspconfig.svelte.setup {
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- نفس الـ keymaps اللي عرفتها تحت
+          local opts = { buffer = bufnr, silent = true }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          -- ... البقية
+          
+          -- حركة ذكية: تحديث الـ TS عند حفظ ملفات السيفلت
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            callback = function(ctx)
+              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+            end,
+          })
+        end,
+      }
+
+      -- 2. تعديل vtsls ليدعم Svelte
       lspconfig.vtsls.setup {
         capabilities = capabilities,
-        -- إجبار السيرفر يشوف المجلد الحالي كشغل واحد
         root_dir = lspconfig.util.root_pattern("package.json", ".git") or vim.loop.cwd(),
         settings = {
           vtsls = {
             autoUseWorkspaceTsdk = true,
             experimental = {
               completion = {
-                enableServerSideFuzzyMatch = true, -- تم تصحيح الكلمة هون
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+            -- تفعيل دعم ملفات سيفلت داخل vtsls
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = "typescript-svelte-plugin",
+                  location = vim.fn.expand("$PNPM_HOME/lib/node_modules/typescript-svelte-plugin"), -- تأكد من المسار حسب pnpm عندك
+                  enableForWorkspaceTypeScriptVersions = true,
+                },
               },
             },
           },
-          javascript = {
-            suggest = {
-              autoImports = true,
-            },
-            updateImportsOnFileMove = { enabled = "always" },
-          },
+          -- ... باقي إعدادات javascript اللي حطيتها
         },
-        on_attach = function(client, bufnr)
-          local opts = { buffer = bufnr, silent = true }
-          -- الاختصارات الأساسية
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
-          vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-          vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { buffer = bufnr, desc = "Rename Symbol" })
-        end,
+        -- ... نفس الـ on_attach
       }
     end,
   },
